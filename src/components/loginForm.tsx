72cb4ecde8615b217362ea7df346d66ad24d5aa3
April Hall <ari@arithefirst.com>
2025-04-29 23:31:31 -0400

    feat: Login page

---
+++ src/components/loginForm.tsx
@@ -0,0 +1,30 @@
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+export function LoginForm({ className, ...props }: React.ComponentPropsWithoutRef<'form'>) {
+  return (
+    <form className={cn('flex flex-col gap-6', className)} {...props}>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold">Login to your account</h1>
+        <p className="text-muted-foreground text-sm text-balance">
+          Enter your username below to login to your account
+        </p>
+      </div>
+      <div className="grid gap-6">
+        <div className="grid gap-2">
+          <Label htmlFor="username">Username</Label>
+          <Input id="username" type="text" placeholder="jane_doe" required />
+        </div>
+        <div className="grid gap-2">
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" type="password" placeholder="MySuperS3curePassw0rd!" required />
+        </div>
+        <Button type="submit" className="w-full">
+          Login
+        </Button>
+      </div>
+    </form>
+  );
+}
